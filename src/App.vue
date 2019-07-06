4151b2481c2e6ada3c95e4e14554165b4f0558d9
Ronnie <941285782@qq.com>
2019-07-06 15:51:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container fluid pl-0 pr-0 pt-0 pb-0>
+    <v-layout align-center justify-center class="container">
+      <v-flex xs12 class="box">
+        hello world!
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.container
+  width 100%
+  height calc(100vh)
+  background-color aqua
+  .box
+    text-align center
+</style>
